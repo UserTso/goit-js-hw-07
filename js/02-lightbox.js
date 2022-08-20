c1adaa7befac0c59f4fda56600c79f2156bd3107
User Tso <tso20101983@gmail.com>
2022-08-21 00:44:47 +0300

    fixed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 const galleryBox = document.querySelector('.gallery');
 const galleryMarkup = createPhotoGalleryMarkup(galleryItems);
 
@@ -9,14 +9,14 @@ function createPhotoGalleryMarkup(galleryItems) {
 
     return galleryItems.map(({preview, original, description}) => {
         return `
-   
+  
     <a class="gallery__item" 
         href="${original}">
     <img class="gallery__image" 
         src="${preview}" 
             alt="${description}" />
     </a>
-
+  
 `;
     }).join('');
 };
@@ -29,5 +29,5 @@ galleryBox.addEventListener('click', onGalleryBoxClick);
 
 function onGalleryBoxClick(event) {
   event.preventDefault();
- var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt'});
+ var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 };
